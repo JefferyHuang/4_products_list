3208e7c4638d6fc5b1f89616dd18715c50fb7184
JefferyHuang <107784099+JefferyHuang@users.noreply.github.com>
2022-06-28 21:46:52 +0800

    Add files via upload

--- products.py
+++ products.py
@@ -3,7 +3,7 @@ while True:
     name = input("請輸入商品名稱: ")
     if name == "q":
         break
-    price = int(input("請輸入商品價格: "))
+    price = input("請輸入商品價格: ")
     products.append([name, price])
     #p = []
     #p.append(name)
@@ -13,3 +13,7 @@ print(products)
 
 for product in products:
     print(product[0], "的價格是", product[1])
+
+with open("products.csv", "w", encoding = "Big5") as f:
+    for product in products:
+        f.write(product[0] + "," + product[1] + "\n")
